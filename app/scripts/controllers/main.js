f40f8bd772350a91847a5092ad4d48123f107829
allenjprice <allenjprice@gmail.com>
2014-10-05 11:34:54 -0700

    Dropdown options stored in arrays and use ng-options. Also: room lists change based on chosen campus. Filled in room lists.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,6 +9,11 @@
  */
 angular.module('uhohApp')
   .controller('MainCtrl', function ($scope) {
+    $scope.campuses = ['Benton', 'Cabot', 'Little Rock'];
+    $scope.LRrooms = ['Worship Center', 'Warehouse', 'Chapel', 'FSM Auditorium', 'FSM Garage', '56 Arena', 'Living Room', 'FSM Green Room', 'Green Room', 'The Station', 'Teaching Pastor\'s Conference Room', 'Upstairs Conference Room', 'FSK Riptide', 'Grandma\'s Garden', 'Paradise Pond', 'Mission Earth', 'HMS Gospel', 'In Search of the Secret Door', 'Knights of the Cross', 'PreSchool Worship'];
+    $scope.BNrooms = ['Worship Center', 'Adult Classroom', 'FSK Worship Room', 'The Barn'];
+    $scope.CBrooms = ['Worship Center', '56 Garage', 'FSK 1st/2nd Grade Room', 'FSK 3rd/4th Grade Room'];
+
     $scope.form = {
       campus: '',
       room: '',
